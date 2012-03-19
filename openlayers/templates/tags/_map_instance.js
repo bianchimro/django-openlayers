a59f878a72039095ec339b1d690d21288300a4a1
Mauro Bianchi <bianchimro@gmail.com>
2012-03-20 00:52:11 +0100

    moving towards templatetags

---
+++ openlayers/templates/tags/_map_instance.js
@@ -0,0 +1,22 @@
+new OpenLayers.Map('map-{{ map.id }}',{
+        controls : [    
+                    {% if map.navigation_control %}
+                        new OpenLayers.Control.Navigation(),
+                    {% endif %}
+                    {% if map.pan_zoom_bar_control %}
+                        new OpenLayers.Control.PanZoomBar(),
+                    {% endif %}
+                    {% if map.layer_switcher_control %}
+                        new OpenLayers.Control.LayerSwitcher({'ascending':false}),
+                    {% endif %}
+                        //new OpenLayers.Control.Permalink(),
+                    {% if map.scale_line_control %}
+                        new OpenLayers.Control.ScaleLine(),
+                    {% endif %}
+                        //new OpenLayers.Control.Permalink('permalink'),
+                        //new OpenLayers.Control.MousePosition(),
+                        //new OpenLayers.Control.OverviewMap(),
+                        //new OpenLayers.Control.KeyboardDefaults()
+                    ]
+        }
+    );
--- openlayers/templatetags/openlayers.py
+++ openlayers/templatetags/openlayers.py
@@ -14,3 +14,23 @@ def contains(value, arg):
   
   return arg in value 
 
+
+
+@register.inclusion_tag('tags/_div_for_map.html')
+def div_for_map(map):
+    '''
+    Inclusion tag sample
+    '''
+    return {'map':map}
+    
+@register.simple_tag
+def map_var(map):
+    
+    return "map" + str(map.id)
+    
+@register.inclusion_tag('tags/_map_instance.js')
+def map_instance(map):
+    '''
+    Inclusion tag sample
+    '''
+    return {'map':map}
